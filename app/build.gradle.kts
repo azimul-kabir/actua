bdd92875ea07c3ff2759b708c80076a402d3f0a0
Azimul Kabir Apu <azimul.kabir123@gmail.com>
2026-09-11 03:52:19 +0600

    Prepare beta.9 for F-Droid-compatible distribution
    * Disable dependency metadata rejected by F-Droid
    
    * Prepare beta.9 F-Droid-compatible release
    
    * docs: add beta.9 store changelog
    
    * docs: add beta.9 release notes
    
    * docs: add F-Droid submission metadata template
    
    * docs: add F-Droid submission checklist

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.azimulkabir.actua"
         minSdk = 28
         targetSdk = 37
-        versionCode = 25
-        versionName = "1.0.0-beta.8"
+        versionCode = 26
+        versionName = "1.0.0-beta.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -31,6 +31,12 @@ android {
             keyAlias = releaseKeyAlias
             keyPassword = releaseKeyPassword
         }
+    }
+
+    // F-Droid rejects the opaque dependency metadata in APK signing blocks.
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
 
     buildTypes {
