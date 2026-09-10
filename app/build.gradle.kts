5f9c73654aa92d8b196d0355b9f11011cbe60b38
Azimul Kabir Apu <azimul.kabir123@gmail.com>
2026-09-11 03:36:18 +0600

    Prepare beta.9 F-Droid-compatible release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.azimulkabir.actua"
         minSdk = 28
         targetSdk = 37
-        versionCode = 25
-        versionName = "1.0.0-beta.8"
+        versionCode = 26
+        versionName = "1.0.0-beta.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
