a233890e8670c9a9c000ec8a23295c625a7a1dd1
Azimul Kabir Apu <azimul.kabir123@gmail.com>
2026-09-10 14:00:14 +0600

    release: prepare 1.0.0-beta.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.azimulkabir.actua"
         minSdk = 28
         targetSdk = 37
-        versionCode = 24
-        versionName = "1.0.0-beta.7"
+        versionCode = 25
+        versionName = "1.0.0-beta.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
