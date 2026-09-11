59ae1acdfa912c351c0502217f74b3d7e92244e3
Azimul Kabir Apu <azimul.kabir123@gmail.com>
2026-09-12 03:13:13 +0600

    release: prepare 1.0.0-beta.12
    Closes #62

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.azimulkabir.actua"
         minSdk = 28
         targetSdk = 37
-        versionCode = 28
-        versionName = "1.0.0-beta.11"
+        versionCode = 29
+        versionName = "1.0.0-beta.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
