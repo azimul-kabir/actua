acff2e63d827eef010b2c4e85fd1fced8c65c898
Azimul Kabir Apu <azimul.kabir123@gmail.com>
2026-09-13 01:04:46 +0600

    fix: use Compose assertion member API

--- app/src/androidTest/java/com/azimulkabir/actua/ui/settings/SettingsLocationPrivacyTest.kt
+++ app/src/androidTest/java/com/azimulkabir/actua/ui/settings/SettingsLocationPrivacyTest.kt
@@ -1,7 +1,6 @@
 package com.azimulkabir.actua.ui.settings
 
 import androidx.activity.ComponentActivity
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
