448eba8be60bacd36b37d2e633f933ef1748573e
Azimul Kabir Apu <azimul.kabir123@gmail.com>
2026-09-12 20:09:43 +0600

    release: prepare 1.0.0-beta.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.azimulkabir.actua"
         minSdk = 28
         targetSdk = 37
-        versionCode = 31
-        versionName = "1.0.0-beta.14"
+        versionCode = 32
+        versionName = "1.0.0-beta.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
